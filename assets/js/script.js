701a146714f46b77fc115091dc9498e9782d3631
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2024-10-29 17:57:46 +0700

    button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+// header 
 const menu = document.querySelector(".header .an");
 const iconMenu = document.querySelector(".header #togger");
 iconMenu.addEventListener('click', show);
 
 function show() {
   if (menu.style.display == 'block') {
-    
+
     menu.style.display = 'none';
   } else {
     menu.style.display = 'block';
@@ -17,7 +18,20 @@ document.addEventListener('click', function (event) {
     menu.style.display = 'none';
   }
 });
-
+// end header 
+// button section1 muaxe 
+let buttonBuyCar = document.querySelectorAll(".Buy-Car")
+for (let i = 0; i < buttonBuyCar.length; i++) {
+  buttonBuyCar[i].addEventListener('click',function(){
+    window.location.href='Buy-Car.html';
+  })
+}
+let buttonSaleCar = document.querySelector(".sales-car");
+buttonSaleCar.addEventListener('click',function(){
+  window.location.href='call-selling.html'
+})
+// end button section1 mua xe 
+// section 4
 const sec4List1 = document.querySelector(".list-1");
 const sec4List2 = document.querySelector(".list-2");
 const sec4List3 = document.querySelector(".list-3");
@@ -40,6 +54,7 @@ function show1() {
   sec4List2.classList.remove('active');
 
 }
+// end section 4 
 
 
 // section5-feedback 
@@ -49,13 +64,13 @@ let feedback2 = document.querySelector(".section-5 #feedback-content2");
 let feedback3 = document.querySelector(".section-5 #feedback-content3");
 let feedback4 = document.querySelector(".section-5 #feedback-content4");
 
-var feedback = [feedback0, feedback1, feedback2, feedback3,feedback4];
+var feedback = [feedback0, feedback1, feedback2, feedback3, feedback4];
 var currentIndex = 0;
 function changeFeedback() {
   feedback[currentIndex].style.display = 'none';
-  currentIndex = (currentIndex + 1) ;
-  if(currentIndex > feedback.length-1){
-    currentIndex  = 0;
+  currentIndex = (currentIndex + 1);
+  if (currentIndex > feedback.length - 1) {
+    currentIndex = 0;
   }
   feedback[currentIndex].style.display = 'block';
 }
